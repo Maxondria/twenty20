d940ca893163cbfb6d85b61e18477a6786c35a18
Davis Junior <mdmad7@gmail.com>
2020-05-17 16:16:29 +0000

    ++init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const FETCH_CONTRIBUTIONS = gql`
+  {
+    getContributions {
+      id
+      message
+      amount
+      updatedAt
+      createdAt
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(FETCH_CONTRIBUTIONS);
+  console.log(data, loading, error);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getContributions.map(({ id, amount, message }) => (
+    <div key={id}>
+      <p>{amount}</p>
     </div>
-  );
-}
+  ));
+};
 
 export default App;
